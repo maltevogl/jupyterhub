bec58c2ac052074b1de062a350d4d01512a97e3d
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2016-12-01 16:32:55 +0100

    next integr

--- examples/postgres-checkpoints/hub/jupyter_notebook_config.py
+++ examples/postgres-checkpoints/hub/jupyter_notebook_config.py
@@ -12,27 +12,27 @@ from pgcontents import PostgresCheckpoints
 # and link to ~/Notebooks for every user.
 #######
 
-credpath = os.path.expanduser('~') + '/.davfs2/secrets'
+#credpath = os.path.expanduser('~') + '/.davfs2/secrets'
 nbpath = os.path.expanduser('~') + '/Notebooks'
 
-if not os.path.exists(nbpath):
-    print('Creating mount point.')
-    os.mkdir(nbpath)
+#if not os.path.exists(nbpath):
+#    print('Creating mount point.')
+#    os.mkdir(nbpath)
 
-def get_username():
-    return pwd.getpwuid( os.getuid() )[ 0 ]
+#def get_username():
+#    return pwd.getpwuid( os.getuid() )[ 0 ]
 
-if os.path.exists(credpath) and os.stat(credpath).st_size != 0:
-    pass
-else:
-    if not os.path.exists(credpath):
-        os.mkdir(os.path.expanduser('~') + '/.davfs2')
-    with open(credpath, 'w+') as out:
-        username = get_username()
-        print("Nice to meet you " + username + "!")
-        password = username + '_pwd'
-        out.write(nbpath + ' ' + 'openidconnect__' + username + ' ' + password)
-    subprocess.call(['chmod', '600', credpath])
+#if os.path.exists(credpath) and os.stat(credpath).st_size != 0:
+#    pass
+#else:
+#    if not os.path.exists(credpath):
+#        os.mkdir(os.path.expanduser('~') + '/.davfs2')
+#    with open(credpath, 'w+') as out:
+#        username = get_username()
+#        print("Nice to meet you " + username + "!")
+#        password = username + '_pwd'
+#        out.write(nbpath + ' ' + 'openidconnect__' + username + ' ' + password)
+#    subprocess.call(['chmod', '600', credpath])
 
 subprocess.call(['mount',nbpath])
 
--- examples/postgres-checkpoints/hub/startup.sh
+++ examples/postgres-checkpoints/hub/startup.sh
@@ -15,13 +15,15 @@ IFS="
 for line in `cat userlist`; do
   test -z "$line" && continue
   user=`echo $line | cut -f 1 -d' '`
-  #echo "adding user $user"
+  useruuid = $(uuidgen)
   useradd -m -G davfs2,fuse -s /bin/bash $user
   mkdir /home/$user/LocalData
-  chown -R $user /home/$user/LocalData
+  mkdir /home/$user/Notebooks
+  mkdir /home/$user/.davfs2
+  echo "/home/$user/Notebooks openidconnect__$user $useruuid" > /home/$user/.davfs2/secrets
+  curl --user admin:$NXTC_ADMIN_PWD $NXTC_API/users/ -d "userid=openidconnect__$user&password=$useruuid"
   sed -i "\$a$WEBDAV_HOST /home/$user/Notebooks davfs user,rw,noauto 0 0" /etc/fstab
-  # reading variables in '' is also a problem
-  #sed -i s/username/$user/g /etc/fstab
+  chown -R $user /home/$user
 done
 
 echo "Init database"
