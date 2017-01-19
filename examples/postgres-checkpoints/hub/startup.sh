b01ebe6a5fec7524be64260454ae3417554273c3
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2017-01-19 15:54:52 +0100

    added osc user api request header

--- examples/postgres-checkpoints/hub/startup.sh
+++ examples/postgres-checkpoints/hub/startup.sh
@@ -23,7 +23,7 @@ for line in `cat userlist`; do
   echo "/home/$user/Notebooks openidconnect__$user $useruuid" > /home/$user/.davfs2/secrets
   chown $user /home/$user/.davfs2/secrets
   chmod 600 /home/$user/.davfs2/secrets
-  /usr/bin/curl -u $ADMIN_USER:$ADMIN_PASSWORD --data "userid=openidconnect__$user&password=$useruuid"  $NXTC_API/users
+  /usr/bin/curl -H "OCS-APIRequest: true" -u $ADMIN_USER:$ADMIN_PASSWORD --data "userid=openidconnect__$user&password=$useruuid"  $NXTC_API/users
   sed -i "\$a$WEBDAV_HOST /home/$user/Notebooks davfs user,noauto,uid=$user,file_mode=600,dir_mode=700 0 1" /etc/fstab
   chown -R $user /home/$user
 done
