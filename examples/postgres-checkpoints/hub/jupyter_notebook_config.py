fdb43e8c259a270f1e1e5d97e10bafef927ef11d
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2017-02-10 11:45:26 +0100

    changed user creation at startup

--- examples/postgres-checkpoints/hub/jupyter_notebook_config.py
+++ examples/postgres-checkpoints/hub/jupyter_notebook_config.py
@@ -1,9 +1,5 @@
-import sys
-#import getpass
 import subprocess
 import os
-import time
-import pwd
 
 from pgcontents import PostgresCheckpoints
 
@@ -12,32 +8,11 @@ from pgcontents import PostgresCheckpoints
 # and link to ~/Notebooks for every user.
 #######
 
-#credpath = os.path.expanduser('~') + '/.davfs2/secrets'
 nbpath = os.path.expanduser('~') + '/Notebooks'
-
-#if not os.path.exists(nbpath):
-#    print('Creating mount point.')
-#    os.mkdir(nbpath)
-
-#def get_username():
-#    return pwd.getpwuid( os.getuid() )[ 0 ]
-
-#if os.path.exists(credpath) and os.stat(credpath).st_size != 0:
-#    pass
-#else:
-#    if not os.path.exists(credpath):
-#        os.mkdir(os.path.expanduser('~') + '/.davfs2')
-#    with open(credpath, 'w+') as out:
-#        username = get_username()
-#        print("Nice to meet you " + username + "!")
-#        password = username + '_pwd'
-#        out.write(nbpath + ' ' + 'openidconnect__' + username + ' ' + password)
-#    subprocess.call(['chmod', '600', credpath])
 
 subprocess.call(['mount',nbpath])
 
 c = get_config()
-
 
 # Use Postgres for Checkpoints
 c.ContentsManager.checkpoints_class = PostgresCheckpoints
--- examples/postgres-checkpoints/hub/startup.sh
+++ examples/postgres-checkpoints/hub/startup.sh
@@ -4,32 +4,50 @@ echo -e $USERS_LIST > /srv/oauthenticator/userlist
 echo "Adding users:"
 cat /srv/oauthenticator/userlist
 
+# adding admin user for docker
 groupadd fuse
 useradd -m -G shadow,davfs2,fuse -p $(openssl passwd -1 $ADMIN_PASSWORD_HUB) admin
 chown admin /srv/jupyterhub
+
 # sed doesnt work on files with no line
 echo > /etc/fstab
-
 IFS="
 "
 for line in `cat userlist`; do
   test -z "$line" && continue
   user=`echo $line | cut -f 1 -d' '`
+  # generate random password
   useruuid=`uuidgen`
   id -u $user &>/dev/null || useradd -m -G davfs2,fuse -s /bin/bash $user
   mkdir /home/$user/LocalData
-  mkdir /home/$user/Notebooks
+  mkdir /home/$user/Notebooks 
   mkdir /home/$user/.davfs2
+  # write credetials to davfs2 secrets file
   echo "/home/$user/Notebooks openidconnect__$user $useruuid" > /home/$user/.davfs2/secrets
   chown $user /home/$user/.davfs2/secrets
-  chmod 600 /home/$user/.davfs2/secrets
-  /usr/bin/curl -H "OCS-APIRequest: true" -u $ADMIN_USER:$ADMIN_PASSWORD --data "userid=openidconnect__$user&password=$useruuid"  $NXTC_API/users
+  chmod 600 /home/$user/.davfs2/secrets 
+
+  # test if user exists in nextcloud db. 
+  testUser=$(curl -H "OCS-APIRequest: true" -u $ADMIN_USER:$ADMIN_PASSWORD $NXTC_API/users/$user | grep -P '(?<=status\>)[a-z]{2}')
+  testResult="<status>ok</status>"
+  if [ "$testUser"="$testResult" ]
+  then
+    # user exists, change password to new random one
+    echo "User $user exists, skipping..."
+    /usr/bin/curl -X PUT -H "OCS-APIRequest: true" -u $ADMIN_USER:$ADMIN_PASSWORD $NXTC_API/users/$user -d key="password" -d value="$useruuid"
+  else
+    # user does not exist, add user with fixed pattern of username and random passoword
+    echo "Adding user $user to database..."
+    /usr/bin/curl -H "OCS-APIRequest: true" -u $ADMIN_USER:$ADMIN_PASSWORD --data "userid=openidconnect__$user&password=$useruuid"  $NXTC_API/users
+  fi
+  
+  # Allow user to mount with davfs into the home folder
   sed -i "\$a$WEBDAV_HOST /home/$user/Notebooks davfs user,noauto,uid=$user,file_mode=600,dir_mode=700 0 1" /etc/fstab
   chown -R $user /home/$user
 done
 
 echo "Init database"
-sleep 5
+#sleep 5
 #Initializing postgres database with pgcontents
 pgcontents init --no-prompt --db-url "postgresql://pgcontent:$CHECKPOINTS_PASSWORD@jupyter-db:5432/checkpoints"
 
