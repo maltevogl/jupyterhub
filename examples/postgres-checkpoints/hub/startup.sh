d084c479d09b9d3d90d8feecf61115c68ee348e4
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2017-03-10 11:35:03 +0100

    new username schema

--- examples/postgres-checkpoints/hub/startup.sh
+++ examples/postgres-checkpoints/hub/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e $USERS_LIST > /srv/oauthenticator/userlist
 
-openssl s_client -connect $NEXTCLOUD_HOST:443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM > certificate.pem 
+openssl s_client -connect $NEXTCLOUD_HOST:443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM > certificate.pem
 cp certificate.pem /etc/davfs2/certs
 cp certificate.pem /srv/jupyterhub
 cp /etc/certs/ssl.pem /etc/davfs2/certs
@@ -25,16 +25,13 @@ for line in `cat /srv/oauthenticator/userlist`; do
   # generate random password
   useruuid=`uuidgen`
   id -u $user &>/dev/null || useradd -m -G davfs2,fuse -s /bin/bash $user && mkdir -p /home/$user/{LocalData,Notebooks,.davfs2}
-  #mkdir /home/$user/LocalData
-  #mkdir /home/$user/Notebooks 
-  #mkdir /home/$user/.davfs2
   # write credetials to davfs2 secrets file
-  echo "/home/$user/Notebooks openidconnect__$user $useruuid" > /home/$user/.davfs2/secrets
+  echo "/home/$user/Notebooks $user $useruuid" > /home/$user/.davfs2/secrets
   chown $user /home/$user/.davfs2/secrets
-  chmod 600 /home/$user/.davfs2/secrets 
+  chmod 600 /home/$user/.davfs2/secrets
 
-  # test if user exists in nextcloud db. 
-  testUser=$(/usr/bin/curl --cacert certificate.pem -H "OCS-APIRequest: true" -u $ADMIN_USER:$ADMIN_PASSWORD $NXTC_API/users/openidconnect__$user | grep -P -o '(?<=.status\>)[a-z]{2}(?=\<\/status\>)')
+  # test if user exists in nextcloud db.
+  testUser=$(/usr/bin/curl --cacert certificate.pem -H "OCS-APIRequest: true" -u $ADMIN_USER:$ADMIN_PASSWORD $NXTC_API/users/$user | grep -P -o '(?<=.status\>)[a-z]{2}(?=\<\/status\>)')
   testResult="ok"
   printf "'$testUser'\n"
   printf "'$testResult'\n"
@@ -43,13 +40,13 @@ for line in `cat /srv/oauthenticator/userlist`; do
   then
     # user exists, change password to new random one
     echo "User '$user' exists, skipping..."
-    /usr/bin/curl --cacert certificate.pem -X PUT -H "OCS-APIRequest: true" -u $ADMIN_USER:$ADMIN_PASSWORD $NXTC_API/users/openidconnect__$user -d key="password" -d value="$useruuid" > /dev/null
+    /usr/bin/curl --cacert certificate.pem -X PUT -H "OCS-APIRequest: true" -u $ADMIN_USER:$ADMIN_PASSWORD $NXTC_API/users/$user -d key="password" -d value="$useruuid" > /dev/null
   else
     # user does not exist, add user with fixed pattern of username and random passoword
     echo "Adding user $user to database..."
-    /usr/bin/curl --cacert certificate.pem -H "OCS-APIRequest: true" -u $ADMIN_USER:$ADMIN_PASSWORD --data "userid=openidconnect__$user&password=$useruuid"  $NXTC_API/users > /dev/null
+    /usr/bin/curl --cacert certificate.pem -H "OCS-APIRequest: true" -u $ADMIN_USER:$ADMIN_PASSWORD --data "userid=$user&password=$useruuid"  $NXTC_API/users > /dev/null
   fi
-  
+
   # Allow user to mount with davfs into the home folder
   sed -i "\$a$WEBDAV_HOST /home/$user/Notebooks davfs user,noauto,uid=$user,file_mode=600,dir_mode=700 0 1" /etc/fstab
   chown -R $user /home/$user
